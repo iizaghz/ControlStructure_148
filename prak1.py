644955435dfe156d1d2758371af3e65a5771f732
iizaghz <sukmahawaiza@gmail.com>
2025-10-06 15:41:40 +0700

    praktikum1

---
+++ prak1.py
@@ -0,0 +1,12 @@
+percentage = float(input("Masukkan persentase nilai siswa: "))
+
+if percentage >= 90:
+    print("Excellent performace")
+elif percentage >= 80:
+    print ("Very Good performance")
+elif percentage >= 70:
+    print ("Good performance")
+elif percentage >= 60:
+    print ("Avarage performance")
+else:
+    print ("Need Improvement") 
